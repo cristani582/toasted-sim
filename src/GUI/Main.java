4d1b7e1001a7eb442c07d98a02e1b763dba815ed
cristani <joao.cristani@ufrgs.br>
2025-05-25 01:47:37 -0300

    Tweaking Toasted's main window title

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -4,7 +4,7 @@ import java.text.ParseException;
 
 public class Main {
     public static void main(String[] args) throws ParseException {
-        new MainFrame("TCC | Transient Conduction Calculator");
+        new MainFrame("\uD83C\uDF5E | ToastedSim");
 
 
     /*
