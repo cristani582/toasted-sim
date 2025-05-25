8243a3c36c507e371f93bea9da49994de6b1d754
cristani <joao.cristani@ufrgs.br>
2025-05-25 01:48:11 -0300

    removing unused lines

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -5,20 +5,5 @@ import java.text.ParseException;
 public class Main {
     public static void main(String[] args) throws ParseException {
         new MainFrame("\uD83C\uDF5E | ToastedSim");
-
-
-    /*
-
-        this.lcInput = lcInput;
-        this.kInput = kInput;
-        this.rhoInput = rhoInput;
-        this.cpInput = cpInput;
-        this.hInput = hInput;
-        this.temperatureProblemInput = temperatureProblemInput;
-        this.temperatureInitialInput = temperatureInitialInput;
-        this.temperatureInfiniteInput = temperatureInfiniteInput;
-        this.dimensionProblemInput = dimensionProblemInput;
-        this.timeSpentInput = timeSpentInput;
-        */
     }
 }
